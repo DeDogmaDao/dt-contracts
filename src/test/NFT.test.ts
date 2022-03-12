72dc734601d3af03c13144dfac2ff92a3bfa00c0
FlokiBB <FlokiBB@protonmail.com>
2022-03-13 02:09:35 +0330

    Feature(Contract): add constructor and owner test

--- src/test/NFT.test.ts
+++ src/test/NFT.test.ts
@@ -60,6 +60,52 @@ describe('NFT', function () {
         )) as NFT;
     });
 
-    
+    describe('#constructor', () => {
+        it('should have correct max supply', async () => {
+            const maxSupply = await NFTContract.MaxSupply();
+            expect(maxSupply).to.equal(maxSupply_);
+        });
 
+        it('should have correct owner address', async () => {
+            const OwnableOwner = await NFTContract.owner();
+            const AddressesOwner = (await NFTContract.ADDRESS()).Owner
+            expect(OwnableOwner).to.equal(ownerAddress);
+            expect(AddressesOwner).to.equal(ownerAddress);
+        });
+
+        it('should have correct platform multisig address', async () => {
+            const platformMultisig = (await NFTContract.ADDRESS()).Platform;
+            expect(platformMultisig).to.equal(platformMultisigAddress);
+        });
+
+        it('should have correct defi titan address', async () => {
+            const defiTitan = (await NFTContract.ADDRESS()).DefiTitan;
+            expect(defiTitan).to.equal(defiTitanAddress);
+        });
+
+        it('should have correct buy back treasury contract address', async () => {
+            const buyBackTreasuryContract = (await NFTContract.ADDRESS()).BuyBackTreasury;
+            expect(buyBackTreasuryContract).to.equal(buyBackTreasuryContractAddress);
+        });
+
+        it('should have correct white list verifier address', async () => {
+            const whiteListVerifier = (await NFTContract.ADDRESS()).WhiteListVerifier;
+            expect(whiteListVerifier).to.equal(whiteListVerifierAddress);
+        });
+
+        it('should have correct royalty distributor address', async () => {
+            const royaltyDistributor = (await NFTContract.ADDRESS()).RoyaltyDistributor;
+            expect(royaltyDistributor).to.equal(royaltyDistributorAddress);
+        });
+
+        it('should have correct god cid', async () => {
+            const godCID = (await NFTContract.IPFS()).GodCID;
+            expect(godCID).to.equal(godCID_);
+        });
+
+        it('should have correct not revealed art cid', async () => {
+            const notRevealedArtCID = (await NFTContract.IPFS()).NotRevealedArtCID;
+            expect(notRevealedArtCID).to.equal(notRevealedArtCID_);
+        });
+    });
 });
